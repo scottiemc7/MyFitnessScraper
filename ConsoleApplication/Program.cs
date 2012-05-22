0abf117e1122de0946403927c4485d58605762a5
Scott McClure <scottie.mcclure@gmail.com>
2012-05-22 17:56:10 -0400

    Adding content

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyFitnessScraper;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName, password, filePath;
+            DateTime startDate = DateTime.Now.Date;
+            int numDays = 1;
+            if (args.Length == 0)
+            {
+                try
+                {
+                    //Ask for args
+                    Console.Write("User Name: ");
+                    userName = Console.ReadLine();
+                    Console.Write("Password: ");
+                    password = Console.ReadLine();
+                    Console.Write("Start Date: ");
+                    startDate = DateTime.Parse(Console.ReadLine());
+                    Console.Write("Total Number Of Days: ");
+                    numDays = Int32.Parse(Console.ReadLine());
+                    Console.Write("Save To File ([Path][FileName].csv): ");
+                    filePath = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(String.Format("Argument exception {0}", ex.Message));                    
+                    return;
+                }//end try/catch
+            }
+            else if (args.Length == 5)
+            {
+                try
+                {
+                    userName = args[0];
+                    password = args[1];
+                    startDate = DateTime.Parse(args[2]);
+                    numDays = Int32.Parse(args[3]);
+                    filePath = args[4];
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(String.Format("Argument exception {0}", ex.Message));
+                    return;
+                }//end try/catch
+            }
+            else
+            {
+                Console.WriteLine("Invalid Arguments, usage should be: {UserName} {Password} {StartDate} {NumberOfDays} {PathToFile}");
+                return;
+            }//end if
+
+            HttpDataGrabber dg = new HttpDataGrabber(new HttpWebRequestFactory(), userName, password);
+            HtmlDataParser pg = new HtmlDataParser();
+
+            Console.WriteLine("Grabbing Data...");
+
+            List<IMeal> allMeals = new List<IMeal>();
+            for (int i = 0; i < numDays; i++)
+            {
+                DateTime date = startDate.AddDays(i);
+                allMeals.AddRange(pg.ParseFoodDiaryDate(dg.GrabFoodDataForDate(date), date));
+            }//end for
+
+            Console.WriteLine("Writing Data...");
+
+            CSVDataFormatter formatter = new CSVDataFormatter();
+            string formattedString = formatter.Format(allMeals);
+
+            try
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine(formattedString);
+                }//end using
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(String.Format("Argument exception {0}", ex.Message));
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyFitnessScraper_Tests/HtmlDataParser_Test.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MyFitnessScraper;
+using System.Reflection;
+using System.IO;
+using System.Diagnostics;
+
+namespace MyFitnessScraper_Tests
+{
+    [TestFixture]
+    public class HtmlDataParser_Test
+    {
+        private readonly string _html;
+        public HtmlDataParser_Test() 
+        { 
+            //Grab our html sample page
+            using (Stream s = Assembly.GetExecutingAssembly().GetManifestResourceStream("MyFitnessBuddy_Tests.SampleHTML.txt"))
+            using (StreamReader sr = new StreamReader(s))
+            {
+                _html = sr.ReadToEnd();
+            }
+        }
+
+        [SetUp]
+        public void Init()
+        { /* ... */ }
+
+        [TearDown]
+        public void Cleanup()
+        { /* ... */ }
+
+        [Test]
+        public void ParseFoodDiary_ParsesCorrect_Number_Of_Meals()
+        {
+            HtmlDataParser parser = new HtmlDataParser();
+
+            //Parse out the data
+            List<IMeal> meals = parser.ParseFoodDiaryDate(_html, DateTime.Now.Date);
+
+            //Our assertions
+            Assert.AreEqual(4, meals.Count);
+        }
+
+
+        [Test]
+        public void ParseFoodDiary_Each_Food_Has_Same_Nutrients()
+        {
+            HtmlDataParser parser = new HtmlDataParser();
+
+            //Parse out the data
+            List<IMeal> meals = parser.ParseFoodDiaryDate(_html, DateTime.Now.Date);
+
+            //Our assertions
+            List<string> firstNutrientNames = new List<string>();
+            foreach (IMeal meal in meals)
+            {
+                foreach (IFood food in meal.Foods)
+                {
+                    if (firstNutrientNames.Count == 0)                    
+                        firstNutrientNames.AddRange(food.Nutrients.Select(p => p.Name));                    
+                    else
+                    {
+                        Assert.AreEqual(firstNutrientNames, food.Nutrients.Select(p => p.Name));
+                    }//end if
+                }//end foreach
+            }//end foreach
+        }
+    }
+}
